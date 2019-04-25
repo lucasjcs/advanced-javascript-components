4b27d7acdd8d03cdf7bc3d518149646b14e8032a
Lucas Jacinto <silval@alunos.utfpr.edu.br>
2019-04-25 17:02:53 -0300

    config]: configurando webpack boilerplate, eslint, prettier e editorconfig

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line no-undef
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+// eslint-disable-next-line no-undef
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
